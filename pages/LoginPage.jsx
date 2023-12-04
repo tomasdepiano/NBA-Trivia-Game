06276aeb0d865376573dd5dc73f1fb1fb986a985
Tomas De Piano <tomasdepiano1@gmail.com>
2023-12-03 22:01:46 -0700

    Added some features on the login page; still need to fix it a lot

--- pages/LoginPage.jsx
+++ pages/LoginPage.jsx
@@ -6,9 +6,25 @@ export default function LoginPage() {
   const isHome = path === "/" ? true : false;
 
   return (
-    <div>
-      {isHome && <>Hello from Login page!</>}
+    <>
+      {isHome && (
+        <>
+          <div>Please Log Into Your Account</div>
+          <form>
+            <label>
+              Email:
+              <input type="text" name="email" />
+            </label>
+            <label>
+              Password:
+              <input type="text" name="password" />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+          <button>Create an Account</button>
+        </>
+      )}
       <Outlet />
-    </div>
+    </>
   );
 }
