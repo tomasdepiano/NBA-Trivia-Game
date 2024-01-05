196cb4e1d426ce3ddf632f1fc6363a92e6ee30ce
Tomas De Piano <tomasdepiano1@gmail.com>
2024-01-05 11:55:18 -0700

    Moved all the quiz data to quiz.jsx file and deleted old game data file

--- scripts/quizdata.js
+++ scripts/quizdata.js
@@ -15,4 +15,73 @@ export const data = [
     option4: "A spearhead",
     ans: 2,
   },
+  {
+    question: "Who was the shortest player to win a MVP?",
+    option1: "Allen Iverson",
+    option2: "Charles Barkley",
+    option3: "Spud Webb",
+    option4: "Steve Nash",
+    ans: 1,
+  },
+  {
+    question:
+      "What was the 'process' of former 76ers general manager Sam Hinkie?",
+    option1: "Looking for players in Europe",
+    option2: "Only drafting excellent 3 point shooters",
+    option3: "Signing veteran leaders",
+    option4: "Tanking for serveral years in a row and getting high draft picks",
+    ans: 4,
+  },
+  {
+    question:
+      "When Kevin Durant won the MVP award in 2014, he said someone else was 'da real MVP'. Who was it?",
+    option1: "Barack Obama",
+    option2: "His mom",
+    option3: "LeBron James",
+    option4: "Russell Westbrook",
+    ans: 2,
+  },
+  {
+    question: "What team used to be the Seattle Supersonics?",
+    option1: "Sacramento Kings",
+    option2: "Memphis Grizzlies",
+    option3: "Oklahoma City Thunder",
+    option4: "Detroit Pistons",
+    ans: 3,
+  },
+  {
+    question: "Who is the all-time NBA leader in assists?",
+    option1: "Chris Paul",
+    option2: "Jason Kidd",
+    option3: "Magic Johnson",
+    option4: "John Stockon",
+    ans: 4,
+  },
+  {
+    question:
+      "Who scored eight points in the final 8.9 seconds to beat the New York Knicks in a 1994 playoff game?",
+    option1: "Penny Hardaway",
+    option2: "Grant Hill",
+    option3: "Scottie Pippen",
+    option4: "Reggie Miller",
+    ans: 4,
+  },
+  {
+    question:
+      "Which NBA legend was known as 'The Admiral' because of his Navy background?",
+    option1: "John Havlicek",
+    option2: "Gary Payton",
+    option3: "David Robinson",
+    option4: "Patrick Ewing",
+    ans: 3,
+  },
+  {
+    question:
+      "What is the record for most regular season wins, set by the Golden State Warriors in 2015-2016?",
+    option1: "50",
+    option2: "69",
+    option3: "73",
+    option4: "82",
+    ans: 3,
+  },
 ];
